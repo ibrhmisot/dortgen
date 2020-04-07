7bd190660e3f2534a65243330d913d0b1552e75f
ibrhmisot <ibrhmisot@gmail.com>
2020-04-07 20:58:08 +0300

    python file

---
+++ daire_alan_hesapla_v1.py
@@ -0,0 +1,12 @@
+def daire_alan_hesapla_v1(yarıçap):
+    print("*"*20)
+
+    pi = 3.14
+    karesi = yarıçap*yarıçap
+    print("r:", yarıçap)
+    sayilari_carp = pi*karesi
+    print("Daire Alanı:",sayilari_carp)
+
+    print("*"*20)
+
+daire_alan_hesapla_v1(4)
---
+++ kosul_deyimi.py
@@ -0,0 +1,21 @@
+yas=int(input("Yaşınız nedir?"))
+
+if yas<100 and yas>50:
+    print("Yaşınız 50'den büyüktür.")
+
+if yas < 50:
+    print("Yaşınız 50'den küçüktür.")
+
+if yas <= 50:
+    print("Yaşınız 0-50 aralığındadır.")
+
+if yas >= 50 and yas <=100:
+    print("Yaşınız 50-100 aralığındadır.")
+
+if yas>100:
+    print("Lütfen 0-100 aralığında bir değer giriniz.")
+    
+
+
+
+  
