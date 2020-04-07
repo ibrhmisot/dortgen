39d601f8914804adaebb526e049c218c271bf701
ibrhmisot <ibrhmisot@gmail.com>
2020-04-07 17:59:58 +0300

    python file

---
+++ dortgen_alan_hesapla_v1.py
@@ -0,0 +1,12 @@
+def dortgen_alan_hesapla_v1(uzun,kisa):
+
+    print("uzun kenar:", uzun)
+    print("kisa kenar:", kisa)
+    sayilari_carp = uzun*kisa
+    print("Alan:", sayilari_carp)
+
+dortgen_alan_hesapla_v1(10,8)
+
+
+
+
